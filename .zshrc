2428cf6e5de52c6c3c75723034d9d3f35d985bf6
David Ko <dko@cainc.com>
2022-03-17 15:04:30 -0400

    add nvm

--- .zshrc
+++ .zshrc
@@ -26,3 +26,7 @@ autoload -Uz compinit && compinit
 # Set up auto-complete when typing partially and pressing UP_ARROW
 bindkey '\e[A' history-search-backward
 bindkey '\e[B' history-search-forward
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
--- bootstrap.sh
+++ bootstrap.sh
@@ -2,17 +2,25 @@
 
 DOTFILES=".dotfiles"
 
+echo ">>> applying .gitconfig"
 # remove top level .gitconfig and replace with a symbolic link 
 rm $HOME/.gitconfig
 ln -s $HOME/$DOTFILES/git/.gitconfig $HOME
 
+echo ">>> applying .zshrc"
 # remove top level .zshrc and replace with a symbolic link 
 rm $HOME/.zshrc
 ln -s $HOME/$DOTFILES/.zshrc $HOME
 
+echo ">>> adding change-bg-color command"
 # remove top level command and update its symbolic link
 rm /usr/local/bin/change-bg-color
 ln -s $HOME/$DOTFILES/scripts/change-bg-color /usr/local/bin
 
+echo ">>> applying git user and email"
 # apply git settings
 source ./git/.git-settings;
+
+echo ">>> downloading nvm"
+# download nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
